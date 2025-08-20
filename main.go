d45ab72e2301be291130948d61d17a11cd6a534b
RavinduSulakshana <ravindusulakshana2525@gmail.com>
2025-08-21 03:57:11 +0530

    implement getmovies and deleteMovie functions

--- main.go
+++ main.go
@@ -25,9 +25,37 @@ type Director struct{
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(movies) 
+}
+
+func deleteMovie(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	params:= mux.Vars(r)
+	
+	for index,item := range movies{
+
+		if item.ID == params["id"]{
+			movies = append(movies[:index],movies[index+1]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie (w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	var movie movies
+	_=json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID=(rand.Intn(10000))
+}
+
 func main(){
 	r := mux.NewRouter()
-	movies =append(movies,Movie{id:"1",Isbn:"43453423",Title:"Movie One",Director:&Director})
+	movies =append(movies,Movie{id:"1",Isbn:"43453423",Title:"Movie One",Director:&Director{Firstname:"John",Lastname:"Joe"}})
+	movies =append(movies,Movie{id:"2",Isbn:"43453343",Title:"Movie Two",Director:&Director{Firstname:"Steve",Lastname:"Smith"}})
+
 	r.HandleFunc("/movies".getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
 	r.HandleFunc("/movies",createMovie).Methods("POST")
